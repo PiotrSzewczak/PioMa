eb6e07a4a78cbf8f12f9f70586e48b6c784466d3
St3llatCh3ff <maciej.oskar.wartak@gmail.com>
2025-12-16 13:48:04 +0100

    Debug Insert

--- run.py
+++ run.py
@@ -9,11 +9,22 @@ addresses_df = pd.read_csv('addresses.csv', sep=';')
 
 # jedno wywołanie geokodowania
 addresses_df = geocode_addresses(addresses_df)
-
+addresses_df = addresses_df.rename(columns={
+    "ID_new": "id",
+    "Ulica": "street",
+    "Numer domu": "house_nr",
+    "Miasto": "city",
+    "Concat": "full_address",
+    "Longitude": "longitude",
+    "Latitude": "latitude"
+})
+# podgląd wyników
+#
 print(addresses_df)
 
 # wrzucenie do bazy
 db = get_database()
-db.insert_dataframe('pioma_proj.apartments_geocoded', addresses_df)
+db.insert_dataframe('pioma_proj.apartments', addresses_df)
 
-input("Press Enter to close")
+
+
